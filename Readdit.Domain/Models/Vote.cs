3143f7631b16fb10ffa587b9bc4108966d378f30
Marcin <marcinkaim96@gmail.com>
2020-02-14 14:03:09 +0100

    Add Vote model

---
+++ Readdit.Domain/Models/Vote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readdit.Domain.Models
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public Link Link { get; set; }
+        public ApplicationUser User { get; set; }
+        public VoteType VoteType { get; set; }
+    }
+}
---
+++ Readdit.Domain/Models/VoteType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readdit.Domain.Models
+{
+    public enum VoteType
+    {
+        Up,
+        Down,
+        NonVote
+    }
+}
