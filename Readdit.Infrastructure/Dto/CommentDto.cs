85d8fec4b088562fbe24c40cfe0a7bff46e4b839
Marcin <marcinkaim96@gmail.com>
2020-02-19 21:14:24 +0100

    Add Dtos

---
+++ Readdit.Infrastructure/Dto/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readdit.Infrastructure.Dto
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ Readdit.Infrastructure/Dto/LinkDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readdit.Infrastructure.Dto
+{
+    public class LinkDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public int Ups { get; set; }
+        public int Downs { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public IEnumerable<CommentDto> Comments { get; set; }
+    }
+}
