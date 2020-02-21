1c80296c3936aaf213ae223cfd092d2468706ab1
Marcin <marcinkaim96@gmail.com>
2020-02-21 13:23:09 +0100

    Add partials for comments

---
+++ Readdit/Models/Links/DetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Readdit.Infrastructure.Application.Comments.Commands;
+using Readdit.Infrastructure.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Readdit.Models.Links
+{
+    public class DetailsViewModel
+    {
+        public LinkDto Link { get; set; }
+        public CreateCommentCommand CreateComment { get; set; }
+    }
+}
