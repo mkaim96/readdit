a1faa51b6c35c7f799b305c5bc4fe219f820dc6a
Marcin <marcinkaim96@gmail.com>
2020-02-21 13:21:54 +0100

    Add command

---
+++ Readdit.Infrastructure/Application/Links/Queries/GetLink/GetLinkById.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Readdit.Infrastructure.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readdit.Infrastructure.Application.Links.Queries.GetLink
+{
+    public class GetLinkById : IRequest<LinkDto>
+    {
+        public int Id { get; set; }
+    }
+}
