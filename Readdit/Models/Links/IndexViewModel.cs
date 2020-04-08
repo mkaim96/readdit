ec8bd8445af747a4745d8e2f06b637b7001e43f6
Marcin <marcinkaim96@gmail.com>
2020-04-08 19:53:02 +0200

    Add paging

---
+++ Readdit/Models/Links/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Readdit.Infrastructure.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Readdit.Models.Links
+{
+    public class IndexViewModel
+    {
+        public IReadOnlyList<LinkDto> Links { get; set; }
+        public int NextPage { get; set; }
+    }
+}
