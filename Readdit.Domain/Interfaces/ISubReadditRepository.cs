ca157f3ba6a51caa3e9699edb7d35fee9524b1dd
Marcin <marcinkaim96@gmail.com>
2020-04-17 00:24:40 +0200

    Add subreaddit repository

---
+++ Readdit.Domain/Interfaces/ISubReadditRepository.cs
@@ -0,0 +1,14 @@
+﻿using Readdit.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readdit.Domain.Interfaces
+{
+    public interface ISubReadditRepository
+    {
+        Task<IReadOnlyCollection<SubReaddit>> Search(string search);
+        Task<IReadOnlyCollection<SubReaddit>> GetPopular();
+    }
+}
---
+++ Readdit.Infrastructure/Repositories/SubReadditRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Domain.Interfaces;
+using Readdit.Domain.Models;
+using Readdit.Infrastructure.Ef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readdit.Infrastructure.Repositories
+{
+    public class SubReadditRepository : ISubReadditRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubReadditRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IReadOnlyCollection<SubReaddit>> GetPopular()
+        {
+            return await _context.SubReaddits.ToListAsync();
+        }
+
+        public async Task<IReadOnlyCollection<SubReaddit>> Search(string search)
+        {
+            return await _context.SubReaddits
+                .Where(x => x.Name.ToLower().Contains(search.ToLower()))
+                .ToListAsync();
+        }
+    }
+}
