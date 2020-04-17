99b716ba3768692f6b0c815a0d7ed1dce8dd0e3a
Marcin <marcinkaim96@gmail.com>
2020-04-17 17:04:50 +0200

    add user to subreaddit model

--- Readdit.Domain/Models/SubReaddit.cs
+++ Readdit.Domain/Models/SubReaddit.cs
@@ -9,11 +9,17 @@ namespace Readdit.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public IEnumerable<Link> Links { get; set; }
+        public ApplicationUser User { get; set; }
 
-        public SubReaddit(string name)
+        public SubReaddit(string name, ApplicationUser user)
         {
             Name = name;
+            User = user;
             Links = new List<Link>();
+        }
+
+        private SubReaddit()
+        {
         }
     }
 }
---
+++ Readdit.Infrastructure/Migrations/20200417150058_addUserToSubReadditModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Readdit.Infrastructure.Migrations
+{
+    public partial class addUserToSubReadditModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "SubReaddits",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubReaddits_UserId",
+                table: "SubReaddits",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubReaddits_AspNetUsers_UserId",
+                table: "SubReaddits",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubReaddits_AspNetUsers_UserId",
+                table: "SubReaddits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubReaddits_UserId",
+                table: "SubReaddits");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "SubReaddits");
+        }
+    }
+}
